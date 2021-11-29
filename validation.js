3ec3dac3e22e51bbb3970df47cd82f6de7b62add
Vaishakhp2001 <vaishakhp11@gmail.com>
2021-11-29 14:22:19 +0530

    initial commit

---
+++ validation.js
@@ -0,0 +1,17 @@
+// const form=document.getElementById("submit-form")
+// const name=document.getElementById("name")
+// const email=document.getElementById("email")
+// const phone=document.getElementById("phone")
+// const message=document.getElementById("message")
+
+// form.addEventListener("submit",(e) => {
+//     e.preventDefault();
+//     checkInputs();
+// })
+// function checkInputs(){
+//     const nameValue=name.value
+//     const emailValue=email.value
+//     const phoneValue=phone.value
+//     const message=message.value
+
+// }
